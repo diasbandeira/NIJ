3f570f2fc4bcd8d5dd47234b76bd526b683de21d
diasbandeira <diasbandeira@gmail.com>
2023-09-19 07:40:48 -0300

    Criação de Acesso Autenticado

---
+++ NIJ.Web/Controllers/InfraController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NIJ.Web.Models.Infra;
+//using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace NIJ.Web.Controllers
+{
+    [Authorize]
+    public class InfraController : Controller
+    {
+        private readonly UserManager<UserAplication> _userManager;
+        private readonly SignInManager<UserAplication> _signInManager;
+        //private readonly ILogger _logger;
+
+        public InfraController(UserManager<UserAplication> userManager, SignInManager<UserAplication> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            //_logger = logger;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Acessar(string returnUrl)
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            ViewData["ReturnUrl"] = returnUrl;
+            
+            return View();
+
+        }
+    }
+}
---
+++ NIJ.Web/Models/Infra/ViewModel/AcessarViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NIJ.Web.Models.Infra.ViewModel
+{
+    public class AcessarViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Lembrar de Mim?")]
+        public bool Remember { get; set; }
+    }
+}
